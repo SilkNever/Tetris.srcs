5ee926d9e1093ba30f7a25b3d653fd62731f0730
SilkNever <981636239@qq.com>
2024-05-27 17:02:35 +0800

    修复了方块落地就被清除的bug

--- sources_1/new/LogicImplement.v
+++ sources_1/new/LogicImplement.v
@@ -250,10 +250,15 @@ module LogicImplement(
                 if (isDown == 1) begin
                     // 到底了就判断一波有没有能消除的行
                     for(k = 19, i = 19; i > 0; i = i - 1) begin
-                        if (~gameArea[i]) begin
+                        if (~gameArea[i] > 0) begin
                             gameArea[k] = gameArea[i];
-                            gameArea[i] = 0;
+                            if (i < k) begin
+                                gameArea[i] = 0;
+                            end
                             k = k - 1;
+                        end
+                        else begin
+                            gameArea[i] = 0;
                         end
                     end
                     gameArea[k] = gameArea[i];
